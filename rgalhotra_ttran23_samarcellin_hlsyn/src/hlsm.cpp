d49b4d239a4e55f4984bba33720ee9f517081905
Duckie88 <steph.marcellin@gmail.com>
2017-11-21 00:46:07 -0700

    pointers should never be done tired. asap dealt with (i'm pretty sure...)

--- rgalhotra_ttran23_samarcellin_hlsyn/src/hlsm.cpp
+++ rgalhotra_ttran23_samarcellin_hlsyn/src/hlsm.cpp
@@ -8,6 +8,7 @@ bool scheduleASAP(int latency, std::vector<node>* unscheduled, std::vector<std::
 	int i = 0;
 	int j,k,m;
 	bool unschedPrevNode = false;
+	std::vector<node*> temp;
 
 	while(scheduled < (*unscheduled).size()){
 		(*ASAP).push_back(std::vector<node>());
@@ -21,41 +22,45 @@ bool scheduleASAP(int latency, std::vector<node>* unscheduled, std::vector<std::
 					(*unscheduled).at(j).setAsapTime(i);
 					scheduled++;
 					
-					//Need to account for the delay if there is one
-					//FIXME~!
-					for (k = 0; k < (*unscheduled).at(j).getNextNodes().size(); k++) {
-						if (i + (*unscheduled).at(j).getDelay() > (*unscheduled).at(j)->getNextNodes().at(k)->getCyclesElapsed()) {
-							(*unscheduled).at(j)->getNextNodes().at(k)->setCyclesElapsed(i + (*unscheduled).at(j).getDelay());
+					//deal with the delay (if exists)
+					temp = (*unscheduled).at(j).getNextNodes();
+					for (k = 0; k < temp.size(); k++) {
+						if (i + (*unscheduled).at(j).getDelay() > temp.at(k)->getCyclesElapsed()) {
+							temp.at(k)->setCyclesElapsed(i + (*unscheduled).at(j).getDelay());
 						}
 					}
 				}
 			}
 		else {
-				//schedule node if allowed cycle is equal to i
-				for (m = 0; m < (*unscheduled).at(j)->getPreviousNodes().size(); m++) {
-					if ((*unscheduled).at(j)->getPreviousNodes().at(m)->getAsapTime() == -1) {
+				//schedule node if cycle is i
+				temp = (*unscheduled).at(j).getPrevNodes();
+				for (m = 0; m < temp.size(); m++) {
+					if (temp.at(m)->getAsapTime() == -1) {
 						unschedPrevNode = true;
 					}
 				}
-				if ((*unscheduled).at(j)->getCycleAllowed() == i && !unschedPrevNode) {
+				if ((*unscheduled).at(j).getCyclesElapsed() == i && !unschedPrevNode) {
 					(*ASAP).at(i).push_back((*unscheduled).at(j));
-					(*unscheduled).at(j)->setAsapTime(i);
+					(*unscheduled).at(j).setAsapTime(i);
 					scheduled++;
-					//update nodes allowed cycle time
-					for (k = 0; k < (*unscheduled).at(j)->getNextNodes().size(); ++k) {
-						if (i + (*unscheduled).at(j)->getDelay() > (*unscheduled).at(j)->getNextNodes().at(k)->getCycleAllowed()) {
-							(*unscheduled).at(j)->getNextNodes().at(k)->setCycleAllowed(i + (*unscheduled).at(j)->getDelay());
+					//update node cycle time
+					for (k = 0; k < (*unscheduled).at(j).getNextNodes().size(); ++k) {
+						if (i + (*unscheduled).at(j).getDelay() > (*unscheduled).at(j).getNextNodes().at(k)->getCyclesElapsed()) {
+							(*unscheduled).at(j).getNextNodes().at(k)->setCyclesElapsed(i + (*unscheduled).at(j).getDelay());
 						}
 					}
-					if ((*unscheduled).at(j)->getConditional()) {
-						for (k = 0; k < (int)(*unscheduled).at(j)->getNextIfNodes().size(); ++k) {
-							if (i + (*unscheduled).at(j)->getDelay() > (*unscheduled).at(j)->getNextIfNodes().at(k)->getCycleAllowed()) {
-								(*unscheduled).at(j)->getNextIfNodes().at(k)->setCycleAllowed(i + (*unscheduled).at(j)->getDelay());
+					if ((*unscheduled).at(j).getConditional()) {
+						temp = (*unscheduled).at(j).getNextIfNodes();
+						for (k = 0; k < (*unscheduled).at(j).getNextIfNodes().size(); ++k) {
+							if (i + (*unscheduled).at(j).getDelay() > temp.at(k)->getCyclesElapsed()) {
+								temp.at(k)->setCyclesElapsed(i + (*unscheduled).at(j).getDelay());
 							}
 						}
-						for (k = 0; k < (int)(*unscheduled).at(j)->getNextElseNodes().size(); ++k) {
-							if (i + (*unscheduled).at(j)->getDelay() > (*unscheduled).at(j)->getNextElseNodes().at(k)->getCycleAllowed()) {
-								(*unscheduled).at(j)->getNextElseNodes().at(k)->setCycleAllowed(i + (*unscheduled).at(j)->getDelay());
+						
+						temp = (*unscheduled).at(j).getNextElseNodes();
+						for (k = 0; k < temp.size(); ++k) {
+							if (i + (*unscheduled).at(j).getDelay() > temp.at(k)->getCyclesElapsed()) {
+								temp.at(k)->setCyclesElapsed(i + (*unscheduled).at(j).getDelay());
 							}
 						}
 					}
