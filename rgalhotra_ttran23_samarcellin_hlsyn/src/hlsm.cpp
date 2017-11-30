70d8a5ad5e2bf10abe70aa88ac6b39385613cfe4
Rohin Galhotra <jiminycracken@gmail.com>
2017-11-30 16:12:57 -0700

    revert to earlier commit

--- rgalhotra_ttran23_samarcellin_hlsyn/src/hlsm.cpp
+++ rgalhotra_ttran23_samarcellin_hlsyn/src/hlsm.cpp
@@ -323,31 +323,27 @@ bool FDS(int totalNodes, int latency, std::vector<node>* nodes){
 							temp += 0;
 						}
 						else {
-							for (x = 0; x < latency; x++) {
-								if (x >= (*nodes).at(i).getNextNodes().at(k)->getAsapTime() && x <= (*nodes).at(i).getNextNodes().at(k)->getAlapTime()) {
-									if (x > j) {
-										if ((*nodes).at(i).getOperation() == "+" || (*nodes).at(i).getOperation() == "-") {
-											nextDist = addDist;
-										}
-										else if ((*nodes).at(i).getOperation() == "*") {
-											nextDist = mulDist;
-										}
-										else if ((*nodes).at(i).getOperation() == "/" || (*nodes).at(i).getOperation() == "%") {
-											nextDist = divDist;
-										}
-										else {
-											nextDist = logicDist;
-										}
-										temp += nextDist.at(x) * (1 - (*nodes).at(i).getNextNodes().at(k)->getProbability());
-										for (int z = (*nodes).at(i).getNextNodes().at(k)->getAsapTime(); z <= (*nodes).at(i).getNextNodes().at(k)->getAlapTime(); z++) {
-											if ((z > j) & (z != x)) {
-												temp = temp + nextDist.at(z) * (0 - (*nodes).at(i).getNextNodes().at(k)->getProbability());
-											}
-										}
+							for (x = 0; (unsigned int)x < nextDist.size(); x++) {
+								if ((*nodes).at(i).getOperation() == "+" || (*nodes).at(i).getOperation() == "-") {
+									nextDist = addDist;
+								}
+								else if ((*nodes).at(i).getOperation() == "*") {
+									nextDist = mulDist;
+								}
+								else if ((*nodes).at(i).getOperation() == "/" || (*nodes).at(i).getOperation() == "%") {
+									nextDist = divDist;
+								}
+								else {
+									nextDist = logicDist;
+								}
+								temp += nextDist.at(x) * (1 - (*nodes).at(i).getNextNodes().at(k)->getProbability());
+								for (int z = (*nodes).at(i).getNextNodes().at(k)->getAsapTime(); z <= (*nodes).at(i).getNextNodes().at(k)->getAlapTime(); z++) {
+									if (z > j) {
+										temp = temp + nextDist.at(z) * (0 - (*nodes).at(i).getNextNodes().at(k)->getProbability());
 									}
 								}
 							}
-						}		
+						}
 						(*nodes).at(i).setSuccForce(j, (*nodes).at(i).getSuccForce().at(j) + temp);  //THIS IS THE LINE THAT USED TO BREAK IT ROHIN - YOU FORGOT TO CREATE THE SPACE IN THE VECTOR
 					}
 					for (k = 0; (unsigned int)k < (*nodes).at(i).getNextIfNodes().size(); k++) {
@@ -356,32 +352,28 @@ bool FDS(int totalNodes, int latency, std::vector<node>* nodes){
 							temp += 0;
 						}
 						else {
-							for (x = 0; x < latency; x++) {
-								if (x >= (*nodes).at(i).getNextIfNodes().at(k)->getAsapTime() && x <= (*nodes).at(i).getNextIfNodes().at(k)->getAlapTime()) {
-									if (x > j) {
-										if ((*nodes).at(i).getOperation() == "+" || (*nodes).at(i).getOperation() == "-") {
-											nextDist = addDist;
-										}
-										else if ((*nodes).at(i).getOperation() == "*") {
-											nextDist = mulDist;
-										}
-										else if ((*nodes).at(i).getOperation() == "/" || (*nodes).at(i).getOperation() == "%") {
-											nextDist = divDist;
-										}
-										else {
-											nextDist = logicDist;
-										}
-										temp += nextDist.at(x) * (1 - (*nodes).at(i).getNextIfNodes().at(k)->getProbability());
-										for (int z = (*nodes).at(i).getNextIfNodes().at(k)->getAsapTime(); z <= (*nodes).at(i).getNextIfNodes().at(k)->getAlapTime(); z++) {
-											if ((z > j) & (z != x)) {
-												temp = temp + nextDist.at(z) * (0 - (*nodes).at(i).getNextIfNodes().at(k)->getProbability());
-											}
-										}
+							for (x = 0; (unsigned int)x < nextDist.size(); x++) {
+								if ((*nodes).at(i).getOperation() == "+" || (*nodes).at(i).getOperation() == "-") {
+									nextDist = addDist;
+								}
+								else if ((*nodes).at(i).getOperation() == "*") {
+									nextDist = mulDist;
+								}
+								else if ((*nodes).at(i).getOperation() == "/" || (*nodes).at(i).getOperation() == "%") {
+									nextDist = divDist;
+								}
+								else {
+									nextDist = logicDist;
+								}
+								temp += nextDist.at(x) * (1 - (*nodes).at(i).getNextIfNodes().at(k)->getProbability());
+								for (int z = (*nodes).at(i).getNextIfNodes().at(k)->getAsapTime(); z <= (*nodes).at(i).getNextIfNodes().at(k)->getAlapTime(); z++) {
+									if (z > j) {
+										temp = temp + nextDist.at(z) * (0 - (*nodes).at(i).getNextIfNodes().at(k)->getProbability());
 									}
 								}
 							}
 						}
-						(*nodes).at(i).setSuccForce(j, (*nodes).at(i).getSuccForce().at(j) + temp);  //THIS IS THE LINE THAT USED TO BREAK IT ROHIN - YOU FORGOT TO CREATE THE SPACE IN THE VECTOR
+						(*nodes).at(i).setSuccForce(j, (*nodes).at(i).getSuccForce().at(j) + temp);
 					}
 				}
 			}
